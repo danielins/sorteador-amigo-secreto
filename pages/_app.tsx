89b2105ade6f315d46f75b717d3e7b87d19ce4c3
Daniel Lins <daniel.alves.321@gmail.com>
2022-08-23 18:20:08 -0300

    added emotion packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AppProps } from '/next/app'
+import { AppProps } from 'next/app'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
