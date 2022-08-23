b5fbbda85187076b6184ca2416d10e719231f771
Daniel Lins <daniel.alves.321@gmail.com>
2022-08-23 18:41:06 -0300

    theme provider added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import { ThemeProvider } from '@emotion/react'
+
+import baseTheme from '../styles/themes/base'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={baseTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
