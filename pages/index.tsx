9ba6be7b56ac92d04f91ef2e151572b9c7378a26
Daniel Lins <daniel.alves.321@gmail.com>
2022-08-23 16:55:58 -0300

    base project

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Sorteador para Amigo Secreto</title>
+      </Head>
+
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+
+export default Home;
