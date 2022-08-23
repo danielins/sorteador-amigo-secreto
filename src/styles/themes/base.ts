ac33bababf25ef544d42cfb2b17ad5cd66dd0e04
Daniel Lins <daniel.alves.321@gmail.com>
2022-08-23 18:31:20 -0300

    theme files added

---
+++ src/styles/themes/base.ts
@@ -0,0 +1,13 @@
+const colors = {
+  brand: {
+    100: '#4B69FD',
+    200: '#FE652B'
+  }
+}
+
+const baseTheme = {
+  background: colors[100],
+  fontFamily: 'Poppins'
+}
+
+export default baseTheme;
